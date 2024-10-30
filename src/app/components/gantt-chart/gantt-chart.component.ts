530fc93181773117d533959a23a1a277379231b4
Sebastian Ortwein <kron@animeland.de>
2024-10-30 17:24:28 +0100

    Initial commit

---
+++ src/app/components/gantt-chart/gantt-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gantt-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './gantt-chart.component.html',
+  styleUrl: './gantt-chart.component.scss'
+})
+export class GanttChartComponent {
+
+}
